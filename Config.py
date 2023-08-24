4a2ad75f1120bd637a640c04fa9aa52b99e37084
Muhammad Sabbir <138793371+STxSABBIR@users.noreply.github.com>
2023-08-25 01:06:17 +0600

    Update and rename Config.py to config.py

--- Config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# Your Telegram Bot API token
-API_TOKEN = "your-api-token"
-
-# Your Telegram API ID and API Hash
-API_ID = "your-api-id"
-API_HASH = "your-api-hash"
---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Your Telegram Bot API token
+API_TOKEN = "6483542043:AAFw6vDYOhrNQXaMKrl5Z-AXmB-odyF3NXg"
+
+# Your Telegram API ID and API Hash
+API_ID = "23434591"
+API_HASH = "e41c98fa058711992b62e35aae553303"
